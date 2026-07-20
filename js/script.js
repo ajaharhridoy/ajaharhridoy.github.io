57226686ffa8d3d2f89a333d93418721791ea2b1
AJAHAR ISLAM HRIDOY <hridoy2307271008@diu.edu.bd>
2026-07-20 15:25:16 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+// ===============================
+// Dark Mode Toggle
+// ===============================
 
+
+const darkBtn = document.getElementById("darkModeBtn");
+
+
+darkBtn.addEventListener("click", function(){
+
+document.body.classList.toggle("dark-mode");
+
+});
