03d6837a899fff337e165fce21ae3056b4cc516e
AJAHAR ISLAM HRIDOY <hridoy2307271008@diu.edu.bd>
2026-07-21 20:25:12 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
-// ===============================
-// Dark Mode Toggle
-// ===============================
-
+// Dark Mode
 
 const darkBtn = document.getElementById("darkModeBtn");
 
+if (darkBtn) {
+    darkBtn.addEventListener("click", function () {
 
-darkBtn.addEventListener("click", function(){
+        document.body.classList.toggle("dark-mode");
 
-document.body.classList.toggle("dark-mode");
+        if(document.body.classList.contains("dark-mode")){
+            darkBtn.innerHTML="☀️ Light Mode";
+        }else{
+            darkBtn.innerHTML="🌙 Dark Mode";
+        }
 
-});
+    });
+}
